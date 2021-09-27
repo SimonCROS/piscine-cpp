8515c75eab42800753add6f4924ca7c037253575
Simon Cros <scros@student.42lyon.fr>
2021-09-27 09:40:33 +0200

    Save

--- m00/ex02/Account.cpp
+++ m00/ex02/Account.cpp
@@ -2,6 +2,16 @@
 #include <iostream>
 #include <iomanip>
 
+Account::Account( void ) : _accountIndex(Account::_nbAccounts), _amount(0), _nbDeposits(0), _nbWithdrawals(0) {
+	Account::_displayTimestamp();
+	std::cout
+		<< "index:" << this->_accountIndex
+		<< ";amount:" << this->_amount
+		<< ";created"
+		<< std::endl;
+	Account::_nbAccounts++;
+}
+
 Account::Account( int initial_deposit ) : _accountIndex(Account::_nbAccounts), _amount(initial_deposit), _nbDeposits(0), _nbWithdrawals(0) {
 	Account::_displayTimestamp();
 	std::cout
