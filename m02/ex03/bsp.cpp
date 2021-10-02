ed2fb98144142d38a4ef53344d7320bced356b7f
Simon Cros <simon.dc.cros@gmail.com>
2021-10-03 01:00:13 +0200

    m02/ex03 : exclude edges

--- m02/ex03/bsp.cpp
+++ m02/ex03/bsp.cpp
@@ -14,8 +14,8 @@ bool	bsp( Point const a, Point const b, Point const c, Point const point)
     d2 = sign(point, b, c);
     d3 = sign(point, c, a);
 
-    has_neg = (d1 < 0) || (d2 < 0) || (d3 < 0);
-    has_pos = (d1 > 0) || (d2 > 0) || (d3 > 0);
+    has_neg = (d1 <= 0) || (d2 <= 0) || (d3 <= 0);
+    has_pos = (d1 >= 0) || (d2 >= 0) || (d3 >= 0);
 
     return !(has_neg && has_pos);
 }
--- m02/ex03/main.cpp
+++ m02/ex03/main.cpp
@@ -3,14 +3,20 @@
 
 int main(void) {
 	Point p1;
-	Point p2(Fixed(0.42f), Fixed(4.2f));
-	Point p3(Fixed(4), Fixed(2));
+	Point p2( 0.42f, 4.2f );
+	Point p3( 4, 2 );
 
-	Point test1(Fixed(0.2f), Fixed(1.9f));
-	Point test2(Fixed(-21), Fixed(42));
+	Point test1( 0.2f, 1.9f );
+	Point test2( -1, 0 );
+	Point test3;
+	Point test4( 0.01f, 0.01f );
+	Point test5( 3.4f, 2 );
 
 	std::cout << "Triangle : " << p1 << " " << p2 << " " << p3 << std::endl;
 	std::cout << "Test " << test1 << " : " << (bsp(p1, p2, p3, test1) ? "true" : "false") << std::endl;
 	std::cout << "Test " << test2 << " : " << (bsp(p1, p2, p3, test2) ? "true" : "false") << std::endl;
+	std::cout << "Test " << test3 << " : " << (bsp(p1, p2, p3, test3) ? "true" : "false") << std::endl;
+	std::cout << "Test " << test4 << " : " << (bsp(p1, p2, p3, test4) ? "true" : "false") << std::endl;
+	std::cout << "Test " << test5 << " : " << (bsp(p1, p2, p3, test5) ? "true" : "false") << std::endl;
 	return 0;
 }
